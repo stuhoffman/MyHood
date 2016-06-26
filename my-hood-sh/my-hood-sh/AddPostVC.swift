7f0a2cb314e0ae2389939e9ca05f2582c744083e
Stuart Hoffman <stuhoffman@users.noreply.github.com>
2016-06-26 15:49:27 -0700

    Added AddPostVC class to the project, reorg of folders.

---
+++ my-hood-sh/my-hood-sh/AddPostVC.swift
@@ -0,0 +1,20 @@
+//
+//  AddPostVC.swift
+//  my-hood-sh
+//
+//  Created by Stuart Hoffman on 6/26/16.
+//  Copyright © 2016 Stuart Hoffman. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+}
